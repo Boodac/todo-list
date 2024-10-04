3cdf35f30e2f663d24328718406319c64856ebaf
boodac <180095661+Boodac@users.noreply.github.com>
2024-10-03 21:51:47 -0400

    add load-icons.js and import Icons to load-icons. init with loadicons in index.js on load.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { css, reset } from "./assets/assets.js";
 import loadIcons from "./modules/load-icons.js";
 
-
-loadIcons());
+loadIcons();
--- src/modules/load-icons.js
+++ src/modules/load-icons.js
@@ -1,5 +1,21 @@
-import Icons from "../assets/assets.js";
+import { Icons } from "../assets/assets.js";
 
 export default function() {
-    console.log(true);
+    elements.addTask.src = Icons.addTask;
+    elements.today.src = Icons.today;
+    elements.upcoming.src = Icons.upcoming;
+    elements.settings.src = Icons.settings;
+    elements.filters.src = Icons.settings;
+    elements.myTasks.src = Icons.myTasks;
 }
+
+const elements = (function() {
+    const addTask = document.querySelector("#addtask");
+    const today = document.querySelector("#today");
+    const upcoming = document.querySelector('#upcoming');
+    const settings = document.querySelector("#settings");
+    const filters = document.querySelector("#filters");
+    const myTasks = document.querySelector("#mytasks");
+
+    return { addTask, today, upcoming, settings, filters, myTasks };
+})();
