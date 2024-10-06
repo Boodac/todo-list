0d0534a527cf569c863e4760a0429c511732f4e6
boodac <180095661+Boodac@users.noreply.github.com>
2024-10-05 23:10:39 -0400

    rename assets.js to index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,7 @@ import settings from "./icons/settings.svg";
 import today from "./icons/today.svg";
 import upcoming from "./icons/upcoming.svg";
 
-export const Icons = {addTask, filters, myTasks, settings, today, upcoming};
+export const Icons = { addTask, filters, myTasks, settings, today, upcoming };
 
 export * as reset from "./resets.css";
 export * as css from "./styles.css";
